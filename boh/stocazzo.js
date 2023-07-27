52a1022b7aac6f5cbb7e1e0902d1ebe3ebc91906
karlitu <giacomo.amgm@gmail.com>
2023-07-27 23:29:19 +0200

    safdsadf

--- boh/stocazzo.js
+++
@@ -1,8 +0,0 @@
-const dispatcher = serverQueue.connection
-    .play(ytdl(song.url))
-    .on("finish", () => {
-      play(guild, serverQueue.songs[0]);
-    })
-    .on("error", error => console.error(error));
-  dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-  serverQueue.textChannel.send(`Start playing: **${song.title}**`);
