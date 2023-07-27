4005adfc1e67cfcdfbff2b925af49219d4b59c56
karlitu <giacomo.amgm@gmail.com>
2023-07-27 20:57:35 +0200

    new message system

--- bot.js
+++ bot.js
@@ -21,30 +21,34 @@ client.login(token)
 client.on("ready", () => {
     console.log("online")
     let connection = null;
+    let ok = null
     
     client.on("messageCreate", async msg => {
-        if (msg.content.startsWith('§')){
-            
-            if (msg.content.endsWith("join")){
-                console.log("joined");
-                const channel = msg.member.voice.channel;
-                console.log(channel.id);
-                connection = joinVoiceChannel({
-                    channelId: channel.id,
-                    guildId: channel.guild.id,
-                    adapterCreator: channel.guild.voiceAdapterCreator,
-                });
-                const ok = f.create_embed(0xff00ff, "VoiceConnection", "bot is connect")
-                msg.channel.send({ embeds: [ok] });
-                connection;
-            };
-            if (msg.content.endsWith("exit")){
-                console.log("exit");
-                const ok = f.create_embed(0xff00ff, "VoiceConnection", "bot is disconnect")
-                msg.channel.send({ embeds: [ok] });
-                connection.destroy();
-            }
-
+        let message = msg.content;
+        if (message.startsWith('§')){
+            message = message.substring(1);
+            switch (message) {
+                case "join":
+                    console.log("joined");
+                    const channel = msg.member.voice.channel;
+                    connection = joinVoiceChannel({
+                        channelId: channel.id,
+                        guildId: channel.guild.id,
+                        adapterCreator: channel.guild.voiceAdapterCreator,
+                    });
+                    ok = f.create_embed(0xff00ff, "VoiceConnection", "bot is connect")
+                    msg.channel.send({ embeds: [ok] });
+                    connection;
+                    break;
+                case "exit":
+                    console.log("exit");
+                    ok = f.create_embed(0xff00ff, "VoiceConnection", "bot is disconnect")
+                    msg.channel.send({ embeds: [ok] });
+                    connection.destroy();
+                        break;
+                default:
+                    break;
+            }    
         }
     });
-});
+})
